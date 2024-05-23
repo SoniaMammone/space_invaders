491110396c80aa18a4b532ab0f6aeb23eed991e2
Sonia Mammone <sonia.mammone95@gmail.com>
2024-05-23 12:05:03 +0200

    necessita la risoluzione di un bug sulla posizione degli alieni nel gioco

--- game2.js
+++ game2.js
@@ -1,17 +1,17 @@
+//MATRICE
 let tileSize = 32;
 let rows = 16;
 let columns = 16;
-
 let board;
 let boardWidth = tileSize * columns; 
-let boardHeight = tileSize * rows; 
+let boardHeight = tileSize * rows;
+//DEFINISCE IL CODICE 2D 
 let context;
-
+//DEFINISCE LE SHIP
 let shipWidth = tileSize*2;
 let shipHeight = tileSize;
 let shipX = tileSize * columns/2 - tileSize;
 let shipY = tileSize * rows - tileSize*2;
-
 let ship = {
     x : shipX,
     y : shipY,
@@ -21,94 +21,87 @@ let ship = {
 
 let shipImg;
 let shipVelocityX = tileSize;
-
+//ALIENS
 let alienArray = [];
 let alienWidth = tileSize*2;
 let alienHeight = tileSize;
 let alienX = tileSize;
 let alienY = tileSize;
 let alienImg;
-
 let alienRows = 2;
 let alienColumns = 3;
 let alienCount = 0; 
 let alienVelocityX = 1; 
-
+//this.alienVelocity = 1; sta puntanto all'oggetto globale window
+//PLAYER
 let bulletArray = [];
 let bulletVelocityY = -10; 
-
 let score = 0;
 let gameOver = false;
+//il costruttore va scritto con la maiuscola
+// function Alien(alienWidth, alienHeight, alienX, alienY, alienImg, alienRows, alienColumns, alienVelocityX, alive){
+//     this.alienWidth = alienWidth;
+//     this.alienHeight = alienHeight;
+//     this.alienX = alienX;
+//     this.alienY = alienY;
+//     this.alienImg = alienImg;
+//     this.alienRows = alienRows;
+//     this.alienColumns = alienColumns;
+//     this.alienVelocityX = alienVelocityX;
+//     this.alive = alive;
+//     this.die = function(){
+//         console.log("Boom");
+//         this.alive = false;
+//     }
+// }
+//quando invoco un costruttore 1) viene creato un nuovo object vuoto 2) il puntatore this viene fatto puntare a 
+//questo oggetto vuoto appena creato 3) viene eseguito il codice del costruttore vongono create le variabile e
+//inizializzate con i parametri passati nel costruttore 4) ritorna l'indirizzo dell'oggetto che ha creato
+// let al = new Alien(tileSize*2, tileSize);
+// al.die();
+// let alWrong = Alien(tileSize*2, tileSize);
 
-window.onload = function() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const difficulty = urlParams.get('difficulty') || 'easy';
-    // let myVar = s != null && s.toLowerCase();
-    // let z = true && false;
-
-    switch(difficulty) {
-        case 'easy':
-            alienColumns = 6;
-            alienRows = 2;
-            alienVelocityX = 1;
-            break;
-        case 'medium':
-            alienColumns = 4;
-            alienRows = 3;
-            alienVelocityX = 1.5;
-            break;
-        case 'hard':
-            alienColumns = 7;
-            alienRows = 6;
-            alienVelocityX = 3;    
-            break;
-        default:
-            alienColumns = 3;
-            alienRows = 2;
-            alienVelocityX = 1;
-    }
-
+window.onload = function(){
     board = document.getElementById("board");
     board.width = boardWidth;
     board.height = boardHeight;
-    context = board.getContext("2d"); 
+    context = board.getContext("2d");
 
     shipImg = new Image();
     shipImg.src = "./ship.png";
-    shipImg.onload = function() {
-        context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.height);    
+    shipImg.onload = function(){
+        context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.height);
     }
 
     alienImg = new Image();
     alienImg.src = "./alien.png";
     createAliens();
-
     requestAnimationFrame(update);
     document.addEventListener("keydown", moveShip);
     document.addEventListener("keyup", shoot);
+
 }
 
 function update() {
     requestAnimationFrame(update);
-
+//TODO: implementare gameOver
     if (gameOver) {
         return;
     }
 
     context.clearRect(0, 0, board.width, board.height);
 
-    
     context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.height);
-
     
     for (let i = 0; i < alienArray.length; i++) {
         let alien = alienArray[i];
+        //da chiedere a Gian per l'alive perchè non è definita
         if (alien.alive) {
             alien.x += alienVelocityX;
 
             if (alien.x + alien.width >= board.width || alien.x <= 0) {
                 alienVelocityX *= -1;
-                //alien.x += alienVelocityX;
+                alien.x += alienVelocityX*2;
 
                 for (let j = 0; j < alienArray.length; j++) {
                     alienArray[j].y += alienHeight;
@@ -145,7 +138,7 @@ function update() {
     if (alienCount == 0) {
         score += alienColumns * alienRows * 100;
         alienColumns = Math.min(alienColumns + 1, columns/2 -2);
-        alienRows = Math.min(alienRows + 1, rows-4);  
+        alienRows = Math.min(alienRows + 1, rows-4);
         if (alienVelocityX > 0) {
             alienVelocityX += 0.2; 
         }
@@ -157,17 +150,20 @@ function update() {
         createAliens();
     }
 
-    context.fillStyle="white";
+    context.fillStyle="white"; //colore del testo
     context.font="16px courier";
     context.fillText(score, 5, 20);
 }
-
+//TODO: fai si che se la ship arrivi al limite x = 0 || board.width 
+//invece che non muoversi più si ritrova al fine opposto
 function moveShip(e) {
     if (gameOver) {
         return;
     }
-
     if (e.code == "ArrowLeft" && ship.x - shipVelocityX >= 0) {
+        //se ci spostiamo a sinistra e abbiamo ancora spazio a sinistra
+        //allora ci sposteremo a sinistra
+        //ship.x è il bordo della nave
         ship.x -= shipVelocityX; 
     }
     else if (e.code == "ArrowRight" && ship.x + shipVelocityX + ship.width <= board.width) {
@@ -175,13 +171,14 @@ function moveShip(e) {
     }
 }
 
-function createAliens() {
-    for (let c = 0; c < alienColumns; c++) {
-        for (let r = 0; r < alienRows; r++) {
+function createAliens(){
+
+    for(let i = 0; i < alienColumns; i++){
+        for(let j = 0; j < alienRows; j++){
             let alien = {
                 img : alienImg,
-                x : alienX + c*alienWidth,
-                y : alienY + r*alienHeight,
+                x : alienX + i*alienWidth,
+                y : alienY + j*alienHeight,
                 width : alienWidth,
                 height : alienHeight,
                 alive : true
@@ -191,16 +188,16 @@ function createAliens() {
     }
     alienCount = alienArray.length;
 }
-
+//TODO: scrivere funzione in cui in HARD si hanno meno bullet e 
+//e quando finiscono bisogna fare un reload e aspettare qualche secondo (set timeout)
 function shoot(e) {
     if (gameOver) {
         return;
     }
-
     if (e.code == "Space") {
         let bullet = {
             x : ship.x + shipWidth*15/32,
-            y : ship.y,
+            y : ship.y, //esce fuori dal bordo della nave
             width : tileSize/8,
             height : tileSize/2,
             used : false
@@ -215,4 +212,25 @@ function detectCollision(a, b) {
            a.y < b.y + b.height &&  
            a.y + a.height > b.y;    
 }
+
+switch(difficulty){
+    case 'easy':
+        alienColumns = 3;
+        alienRows = 2;
+        alienVelocityX = 1;
+        break;
+    case 'medium':
+        alienColumns = 4;
+        alienRows = 3;
+        alienVelocityX = 1.5;
+    case 'hard':
+        alienColumns = 7;
+        alienRows = 6;
+        alienVelocityX = 20;
+    default:
+        alienColumns = 3;
+        alienRows = 2;
+        alienVelocityX = 1;  
+}
+
 
