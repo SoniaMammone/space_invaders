eab55db68521d14ec6bb536b8636f51eddf7460b
Sonia Mammone <sonia.mammone95@gmail.com>
2024-05-23 10:40:15 +0200

    commit game.js

--- game.js
+++ game.js
@@ -32,11 +32,34 @@ let alienRows = 2;
 let alienColumns = 3;
 let alienCount = 0; 
 let alienVelocityX = 1; 
+//this.alienVelocity = 1; sta puntanto all'oggetto globale window
 //PLAYER
 let bulletArray = [];
 let bulletVelocityY = -10; 
 let score = 0;
 let gameOver = false;
+//il costruttore va scritto con la maiuscola
+// function Alien(alienWidth, alienHeight, alienX, alienY, alienImg, alienRows, alienColumns, alienVelocityX, alive){
+//     this.alienWidth = alienWidth;
+//     this.alienHeight = alienHeight;
+//     this.alienX = alienX;
+//     this.alienY = alienY;
+//     this.alienImg = alienImg;
+//     this.alienRows = alienRows;
+//     this.alienColumns = alienColumns;
+//     this.alienVelocityX = alienVelocityX;
+//     this.alive = alive;
+//     this.die = function(){
+//         console.log("Boom");
+//         this.alive = false;
+//     }
+// }
+//quando invoco un costruttore 1) viene creato un nuovo object vuoto 2) il puntatore this viene fatto puntare a 
+//questo oggetto vuoto appena creato 3) viene eseguito il codice del costruttore vongono create le variabile e
+//inizializzate con i parametri passati nel costruttore 4) ritorna l'indirizzo dell'oggetto che ha creato
+// let al = new Alien(tileSize*2, tileSize);
+// al.die();
+// let alWrong = Alien(tileSize*2, tileSize);
 
 window.onload = function(){
     board = document.getElementById("board");
@@ -72,6 +95,7 @@ function update() {
     
     for (let i = 0; i < alienArray.length; i++) {
         let alien = alienArray[i];
+        //da chiedere a Gian per l'alive perchè non è definita
         if (alien.alive) {
             alien.x += alienVelocityX;
 
@@ -114,7 +138,7 @@ function update() {
     if (alienCount == 0) {
         score += alienColumns * alienRows * 100;
         alienColumns = Math.min(alienColumns + 1, columns/2 -2);
-        alienRows = Math.min(alienRows + 1, rows-4);  
+        alienRows = Math.min(alienRows + 1, rows-4);
         if (alienVelocityX > 0) {
             alienVelocityX += 0.2; 
         }
@@ -159,14 +183,16 @@ function createAliens(){
                 alienImg,
                 alienRows : 2,
                 alienColumns : 3, 
-                alienVelocityX : 1
+                alienVelocityX : 1,
+                alive : true
             }
             alienArray.push(alien);
         }
     }
     alienCount = alienArray.length;
 }
-
+//TODO: scrivere funzione in cui in HARD si hanno meno bullet e 
+//e quando finiscono bisogna fare un reload e aspettare qualche secondo
 function shoot(e) {
     if (gameOver) {
         return;
@@ -174,7 +200,7 @@ function shoot(e) {
     if (e.code == "Space") {
         let bullet = {
             x : ship.x + shipWidth*15/32,
-            y : ship.y, //perchè esce fuori dal bordo della nave
+            y : ship.y, //esce fuori dal bordo della nave
             width : tileSize/8,
             height : tileSize/2,
             used : false
@@ -188,6 +214,26 @@ function detectCollision(a, b) {
            a.x + a.width > b.x &&   
            a.y < b.y + b.height &&  
            a.y + a.height > b.y;    
+}
+
+switch(difficulty){
+    case 'easy':
+        alienColumns = 3;
+        alienRows = 2;
+        alienVelocityX = 1;
+        break;
+    case 'medium':
+        alienColumns = 4;
+        alienRows = 3;
+        alienVelocityX = 1.5;
+    case 'hard':
+        alienColumns = 7;
+        alienRows = 6;
+        alienVelocityX = 20;
+    default:
+        alienColumns = 3;
+        alienRows = 2;
+        alienVelocityX = 1;  
 }
 
 
